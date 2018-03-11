aa436432fd843bfbfa283ac7db3e296a87e9d138
Francesco Piscani <stem.apks@gmail.com>
2018-03-11 11:48:55 -0400

    Add files via upload

---
+++ chapter0/02_commenting_programs_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{ 
+
+
+   // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+        Maybe it needs a rewrite. */
+    return 0;
+}
