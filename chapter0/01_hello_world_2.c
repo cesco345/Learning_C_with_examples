568574aeb1dfd4b8aeda55f31d7b41d8cea78f6c
Francesco Piscani <stem.apks@gmail.com>
2018-03-11 11:50:33 -0400

    Update 01_hello_world_2.c

--- chapter0/01_hello_world_2.c
+++ chapter0/01_hello_world_2.c
@@ -1,6 +1,15 @@
+/*
+Program - How to comment a program in C
+Author - Cesco
+Date - 3/04/2018
+*/
+
 #include <stdio.h>
+
 int main(int argc, const char * argv[]) {
-// insert code here...
-printf("Hello, World!\n");
-return 0;
+
+  // insert code here...
+  printf("Hello, World!\n");
+
+  return 0;
 }
