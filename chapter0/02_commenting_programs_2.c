27b6459963db73a4297ff60443c6a59e1b6ba0d6
Francesco Piscani <stem.apks@gmail.com>
2018-03-11 11:55:03 -0400

    Update 02_commenting_programs_2.c

--- chapter0/02_commenting_programs_2.c
+++ chapter0/02_commenting_programs_2.c
@@ -1,13 +1,18 @@
+/*
+Program - How to comment a program in C
+Author - Cesco
+Date - 3/10/2018
+*/
 #include <stdio.h>
 
 int main (int argc, const char * argv[])
 { 
-
-
-   // Print the beginning of the novel
+    // Print the beginning of the novel
     printf("It was the best of times.\n");
     printf("It was the worst of times.\n");
-    /* Is that actually any good?
+    
+   /* Is that actually any good?
         Maybe it needs a rewrite. */
-    return 0;
+    
+   return 0;
 }
