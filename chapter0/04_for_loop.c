4012f12612e20c4c01a2d4571907d27c4d9495f1
Francesco Piscani <stem.apks@gmail.com>
2018-03-12 20:54:04 -0400

    Add files via upload

---
+++ chapter0/04_for_loop.c
@@ -0,0 +1,15 @@
+/*
+Program - Taken from Objective-C Programming-Pre-Course Workbook =For loop= C program 
+Author - Cesco
+Date - 3/10/2018
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    for (int i = 0; i < 12; i++) {
+        printf("%d. The red sox are a cool team.\n", i);
+    }
+    return 0;
+}
